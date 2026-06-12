a6138dd20940de8d6ae26e6015d0daa12ea50f51
CoderQuang <nhatquangdn0510@gmail.com>
2026-06-12 18:27:56 +0700

    hoc-formn

---
+++ baitap-for.js
@@ -0,0 +1,54 @@
+// Dùng for in ra các số từ 1 - 10
+//for (let i = 1; i <= 10; i++) {
+//    console.log(i);
+//}
+
+// Dùng for in ra các số lẻ từ 1 - 10
+//for (let i = 1; i <= 10; i++) {
+//    if (i % 2 !== 0) {
+//       console.log(i);
+//    }
+//}
+
+// Dùng for duyệt qua các số từ 1 - 10 và in ra các số < 5
+// for (let i = 1; i <= 10; i++) {
+//     if (i < 5) {
+//         console.log(i);
+//     }
+// }
+
+// Dùng for duyệt qua các số từ 1 - 10 và in ra số 6
+// for (let i = 1; i <= 10; i++) {
+//     if (i === 6) {
+//         console.log(i);
+//     }
+// }
+
+// Dùng for tính tổng từ 1 - 10
+// let sum = 0;
+// for (let i = 1; i <= 10; i++) {
+//     sum = sum + i;
+// }
+// console.log(sum);
+
+// Dùng for tính tổng theo công thức: s = 1 + 1/n, n là số tự nhiên bất kỳ
+// let s = 0;
+// let n = prompt("Nhập giá trị của n: ");
+// for (let i = 1; i <= n; i++) {
+//     s = s + 1 / i;
+// }
+// console.log(s);
+
+// Cho 3 số tự nhiên viết hàm if else tìm số lớn nhất
+// let a = prompt("Nhập số a: ");
+// let b = prompt("Nhập số b: ");
+// let c = prompt("Nhập số c: ");
+// if (a > b && a > c) {
+//     console.log("Số lớn nhất là: " + a);
+// } else if (b > a && b > c) {
+//     console.log("Số lớn nhất là: " + b);
+// } else if (c > a && c > b) {
+//     console.log("Số lớn nhất là: " + c);
+// } else {
+//     console.log("Có hai hoặc ba số bằng nhau và lớn nhất");
+// }
---
+++ baitap-functions.js
@@ -0,0 +1,51 @@
+// Dùng for in ra các số từ 1 - 10 bằng cách gọi button, click gọi hàm và truyền tham số vào hàm
+// function demso(n) {
+//     for (let i = 1; i <= n; i++) {
+//     console.log(i);
+//     }
+// }
+        
+//Dùng for in ra các số lẻ từ 1-10
+// function sole(n) {
+//     for (let i = 1; i <= n; i++) {
+//         if (i % 2 !== 0) {
+//             console.log(i);
+//         }
+//     }
+// }
+
+//Dùng for duyệt qua các số từ 1-10 và in ra số < 5
+// function timsonhohon5(n) {
+//     for (let i = 1; i <= n; i++) {
+//         if (i < 5) {
+//             console.log(i);
+//         }
+//     
+// }
+
+//Dùng for duyệt qua các số từ 1-10 và in ra số 6
+// function timson6(n) {
+//     for (let i = 1; i <= n; i++) {
+//         if (i === 6) {
+//             console.log(i);
+//         }
+//     }
+// }
+
+// Dùng for tính tổng các số từ 1-10
+// function tong(n) {
+//     let sum = 0;
+//     for (let i = 1; i <= n; i++) {
+//         sum = sum + i;
+//     }
+//     console.log(sum);
+// }
+
+// Dùng for tính tổng theo công thức: s = 1 + 1/n, n là số tự nhiên bất kỳ
+// function tinhTong(n) {
+//     let s = 0;
+//     for (let i = 1; i <= n; i++) {
+//         s = s + 1/i;
+//     }
+//     console.log(s);
+// }
