63f8009945c9f2aa8bc22f67b04c76e0574074ec
nwuang <quangnpn.23itb@vku.udn.vn>
2026-06-16 21:32:30 +0700

    addtocart

---
+++ baitap/frontend/frontend/js/addtocard.js
@@ -0,0 +1,11 @@
+const addToCartButtons = document.querySelectorAll(".add-to-cart");
+
+addToCartButtons.forEach(function(button) {
+    button.addEventListener("click", function(e) {
+        e.preventDefault();
+        console.log("Đã thêm sản phẩm vào giỏ hàng");
+        let objCon = {}
+        
+    });
+});
+
