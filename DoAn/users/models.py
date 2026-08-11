4c6997a3699b472b0c93b13af84e559ab96bfc0d
CoderQuang <nhatquangdn0510@gmail.com>
2026-08-11 18:27:56 +0700

    doan

--- DoAn/users/models.py
+++ DoAn/users/models.py
@@ -6,7 +6,7 @@ class Country(models.Model):
 
     class Meta:
         verbose_name = "Country"
-        verbose_name_plural = "Countrys"
+        verbose_name_plural = "Countries"
 
     def __str__(self):
         return self.name
